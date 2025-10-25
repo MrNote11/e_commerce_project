6d2454df4cdbf451810bf2d852ba88313d98b0ab
Note <aliyahsulaiman3@gmail.com>
2025-10-25 01:19:52 +0100

    git allowed host

--- e_commerce/__init__.py
+++ e_commerce/__init__.py
@@ -1,5 +1,5 @@
 from .celery import app as celery_app
-from __future__ import absolute_import, unicode_literals
+# from __future__ import absolute_import, unicode_literals
 
  
 __all__ = ("celery_app",)
